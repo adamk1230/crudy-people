f8aba87c3cd2f1bb01f0ac40609b237619fb3b14
Adam Kwan <adamk1230@gmail.com>
2023-07-31 18:43:18 -0400

    fixes issue where title bar flashes mui color

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -3,19 +3,30 @@ import { FC } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 interface HeaderProps {
   title: string;
 }
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#004062',
+    },
+  },
+});
+
 export const TitleBar: FC<HeaderProps> = ({ title }) => {
   return (
-    <AppBar position="static" className="bg-prussian-blue">
-      <Toolbar>
-        <Typography variant="h5" component="div" className="font-sans">
-          {title}
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <ThemeProvider theme={theme}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" component="div" className="font-sans">
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 };
